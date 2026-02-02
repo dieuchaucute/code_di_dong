04e62d77a61949f2f810fb4df27326592c847198
dieuchaucute <chaukhenhbp@gmail.com>
2026-02-02 10:29:29 +0700

    b5.state

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [background, setBackground] = useState('green');
+
+  const colors = [
+    {name: 'GREEN', color: 'green'},
+    {name: 'BLUE ', color: 'blue'},
+    {name: 'BROWN ', color: 'brown'},
+    {name: 'YELLOW ', color: 'yellow', textColor:'black'},
+    {name: 'RED ', color: 'red'},
+    {name: 'BLACK ', color: 'black'},
+  ]
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor: background }]}>
+      {colors.map((item, index) => (
+        <TouchableOpacity
+          key={index}
+          style={[styles.button, { backgroundColor: item.color }]}
+          onPress={() => setBackground(item.color)}
+        >
+          <Text style={[styles.buttonText, {color: item.textColor || 'white'}]}>{item.name}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
@@ -13,8 +31,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+  },
+  button: {
+    width: '90%',
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 25,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
   },
 });
